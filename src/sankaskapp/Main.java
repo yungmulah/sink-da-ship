acc9684807bd5671372b72b4b67f2e7dd5c71ac4
yungmulah <44755516+yungmulah@users.noreply.github.com>
2020-01-04 20:48:25 +0100

    Update Main.java

--- src/sankaskapp/Main.java
+++ src/sankaskapp/Main.java
@@ -33,13 +33,23 @@ public class Main implements MenuItem {
 			if (item.getTitle().equals(this.getTitle())) {
 				toExecute = item;
 			}
-
 			System.out.println(index + ". " + item.getTitle());
 			index++;
-
 		}
-		System.out.print("Ditt val: ");
-		int choice = scan.nextInt();
+		int choice;
+		
+		do {
+			System.out.println();
+			System.out.print("Välj ett av alternativen: ");
+			while(!scan.hasNextInt()) {
+				System.out.print("Välj ett av alternativen: ");
+				scan.next();
+				System.out.println();
+			}
+			choice = scan.nextInt();
+		} while(choice < 0 || choice > index-1);
+		
+		System.out.println();
 		toExecute = items.get(choice);
 		toExecute.execute();
 	}
@@ -47,36 +57,35 @@ public class Main implements MenuItem {
 	public static void main(String[] args) {
 		Main mainMenu = new Main("HUVUDMENY");
 		Main playMenu = new Main("SPELMENY");
+		Main statMenu = new Main("SORTERINGSALTERNATIV");
 
 		mainMenu.add(new AbstractMenuItem("Avsluta") {
-
 			public void execute() {
+				System.out.println();
+				System.out.println("Utvecklat av Axel Karlström och David Envall, IT1.");
 				System.exit(0);
 			}
 		});
 		mainMenu.add(new AbstractMenuItem("Spela spelet") {
 			public void execute() {
-
 				playMenu.execute();
 			}
 		});
 		mainMenu.add(new AbstractMenuItem("Spelarstatistik") {
 			public void execute() {
-				Compare.main(args);
-				System.out.println();
-				mainMenu.execute();
+				statMenu.execute();
 			}
 		});
 
 		playMenu.add(new AbstractMenuItem("Tillbaka") {
 			public void execute() {
-				System.out.println();
 				mainMenu.execute();
 			}
 
 		});
 		playMenu.add(new AbstractMenuItem("Spela mot en vän") {
 			public void execute() {
+				System.out.println();
 				PvP.main(args);
 				mainMenu.execute();
 			}
@@ -84,14 +93,42 @@ public class Main implements MenuItem {
 		});
 		playMenu.add(new AbstractMenuItem("Spela mot datorn") {
 			public void execute() {
+				System.out.println();
 				PvE.main(args);
 				mainMenu.execute();
-				System.out.println();
-
 			}
-
 		});
-
+		
+		statMenu.add(new AbstractMenuItem("Tillbaka") {
+			public void execute() {
+				mainMenu.execute();
+			}
+		});
+		statMenu.add(new AbstractMenuItem("Lägst antal skott") {
+			public void execute() {
+				Compare.shotSort();
+				statMenu.execute();
+			}
+		});
+		statMenu.add(new AbstractMenuItem("Högst träffprocent") {
+			public void execute() {
+				Compare.percentSort();
+				statMenu.execute();
+			}
+		});
+		statMenu.add(new AbstractMenuItem("Minst tid per runda") {
+			public void execute() {
+				Compare.timeSort();
+				statMenu.execute();
+			}
+		});
+		statMenu.add(new AbstractMenuItem("Lägst andel av egna flottan som var beskjuten") {
+			public void execute() {
+				Compare.damageSort();
+				statMenu.execute();
+			}
+		});
+		
 		mainMenu.execute();
 
 	}
